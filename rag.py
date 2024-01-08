3b0411fea0bbe632c3a899ccd4506b53aaff7fdb
jingxuan.xjx <jingxuan.xjx@alibaba-inc.com>
2024-01-08 21:02:31 +0800

    add fallback

--- rag.py
+++ rag.py
@@ -13,11 +13,8 @@ os.environ["DASHSCOPE_API_KEY"] = ""
 
 chatLLM = ChatTongyi(
     model="qwen-72b-chat",
-    # top_k=5,
-    # top_p=0.85,
-    # temperature=0.8,
-    # max_length=300,
-)
+    max_retries=0
+).bind(top_k=5, top_p=0.85, temperature=0.8, max_length=300)
 
 system_prompt = '''
 于谦，别名谦哥、于老师、于大爷，喜欢抽烟、喝酒和烫头，是德云社的相声演员，和郭德纲是搭档，还拍过电影，上过春晚。于谦长期混迹于各大论坛和贴吧，了解不同时期的热点话题和流行八卦，熟悉大量的网络词汇，对各种搞笑段子了如指掌，对各种网络俚语信手拈来。于谦时常以自嘲自黑的方式来调侃自己，尤其喜欢引用网络词汇和搞笑段子。
@@ -31,15 +28,16 @@ embeddings = DashScopeEmbeddings(
     model="text-embedding-v2", dashscope_api_key=""
 )
 
-db = FAISS.load_local('vector_db', embeddings, 'geng')
+db = FAISS.load_local('./vector_db/', embeddings, 'geng')
+print('load vec db done.')
 
 # query = "科目三是什么梗"
 # docs = db.similarity_search(query)
 # print(docs)
 
 retriever = db.as_retriever(search_kwargs={
-    "k": 2,
-    # "score_threshold": 0.99
+    # "k": 2,
+    "score_threshold": 1.2
     })
 
 template = """观众的评论可能和以下资料有关，请自行决定是否参考，然后给出回复:
@@ -57,6 +55,7 @@ prompt = ChatPromptTemplate.from_messages([
     ("human", template)
 ])
 
+model = chatLLM.with_fallbacks([RunnableLambda(lambda x: AIMessage(content="*过滤*"))], exceptions_to_handle=(ValueError,))
 
 chain = (
     {
@@ -64,13 +63,13 @@ chain = (
         "question": RunnablePassthrough()
     }
     | prompt
-    | chatLLM
+    | model
     | StrOutputParser()
 )
 
 while True:
     msg = input()
-    docs = db.similarity_search_with_score(msg)
+    docs = db.similarity_search_with_score(msg, score_threshold=1.2)
     print(docs)
-    ans = chain.invoke( msg)
+    ans = chain.invoke(msg)
     print(ans)
