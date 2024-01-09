e083d1d1fc187a7d119fc5c6915787c6037f929f
jingxuan.xjx <jingxuan.xjx@alibaba-inc.com>
2024-01-09 23:39:22 +0800

    add cuiuc db

--- vector_db_batch_update.py
+++ vector_db_batch_update.py
@@ -5,34 +5,33 @@ import gzip
 import io
 import json
 import dashscope
+import oss2
+from oss2.credentials import EnvironmentVariableCredentialsProvider
 from dashscope import BatchTextEmbedding
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import DashScopeEmbeddings
 
-dashscope.api_key = 'sk-'
+#==================
+# os.environ['OSS_ACCESS_KEY_ID'] = ''
+# os.environ['OSS_ACCESS_KEY_SECRET'] = ''
 
-loader = DirectoryLoader('./data/cuiuc/', glob="19[0-9][0-9].txt")
+# dashscope.api_key = ''
 
-docs = loader.load()
-print(len(docs))
+# loader = DirectoryLoader('./data/cuiuc/', glob="*.txt")
 
-# text_splitter = CharacterTextSplitter(
-#     separator="\n\n",
-#     chunk_size=1000,
-#     chunk_overlap=200,
-#     length_function=len,
-#     is_separator_regex=False,
-# )
-
-# documents = text_splitter.split_documents(docs)
-
-# print(documents)
+# docs = loader.load()
+# print(len(docs))
 
 # with open('tmp/docs.txt', 'w') as fp:
 #     for d in docs:
 #         fp.write(d.page_content.replace('\n', '\\n')+'\n')
+
+# auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
+# bucket = oss2.Bucket(auth, 'https://oss-cn-beijing.aliyuncs.com', 'xjx-pub')
+# bucket.put_object_from_file('docs.txt', 'tmp/docs.txt')
+# print('oss upload done.')
 
 # file_url = "https://xjx-pub.oss-cn-beijing.aliyuncs.com/docs.txt"
 
@@ -41,7 +40,9 @@ print(len(docs))
 #     text_type="document")
 # print(result)
 
-
+# text_embeddings = []
+# metadatas = []
+# emb_map = {}
 # if result.status_code == 200:
 #     if result.output.task_status == 'SUCCEEDED':
 #         print(result.output.url)
@@ -50,40 +51,32 @@ print(len(docs))
 #             # 使用 gzip 解压缩
 #             with gzip.open(io.BytesIO(emb_response.content), 'rb') as f:
 #                 content = f.read()
-#                 emb = json.loads(content.strip())
-#                 print(emb)
+#                 embs = content.decode('utf-8').strip().split('\n')
+#                 print(len(embs))
+#                 assert len(embs) == len(docs)
+#                 for i, e in enumerate(embs):
+#                     data = json.loads(e)
+#                     print(i, data['output']['text_index'])
+#                     emb_map[data['output']['text_index']] = data['output']['embedding']
+#             for i in range(len(docs)):
+#                 text_embeddings.append((docs[i].page_content, emb_map[i]))
+#                 metadatas.append(docs[i].metadata)
 
-text_embeddings = []
-metadatas = []
+embeddings = DashScopeEmbeddings(
+    model="text-embedding-v2", dashscope_api_key=""
+)
 
-res_url = "https://dashscope-result-bj.oss-cn-beijing.aliyuncs.com/5fc5c860/2024-01-09/2ef33d78-29f8-4289-acf4-e1273c5d0285_output_1704794062989.txt.gz?Expires=1705053263&OSSAccessKeyId=LTAI5tQZd8AEcZX6KZV4G8qL&Signature=YW4SYPQ5MOpNFGDkbDex%2Fl61SqA%3D"
-emb_response = requests.get(res_url)
-if emb_response.status_code == 200:
-    # 使用 gzip 解压缩
-    with gzip.open(io.BytesIO(emb_response.content), 'rb') as f:
-        content = f.read()
-        embs = content.decode('utf-8').strip().split('\n')
-        print(len(embs))
-        assert len(embs) == len(docs)
-        for i, e in enumerate(embs):
-            data = json.loads(e)
-            print(i, data['output']['text_index'])
-            text_embeddings.append((docs[i].page_content, data['output']['embedding']))
-            metadatas.append(docs[i].metadata)
-
-
-# embeddings = DashScopeEmbeddings(
-#     model="text-embedding-v2", dashscope_api_key="sk-"
-# )
-
-# db = FAISS.from_documents(documents, embeddings)
 
 # db = FAISS.from_embeddings(text_embeddings, embeddings, metadatas=metadatas)
+# print('build index done.')
 
-# db = FAISS.load_local('./vector_db/', embeddings, 'test')
+# db.save_local("vector_db", "cuiuc")
+# print('save local done.')
+#==================
 
-# query = "尼康战歌"
-# docs = db.similarity_search_with_score(query)
-# print(docs)
+db = FAISS.load_local('./vector_db/', embeddings, 'cuiuc')
 
-# db.save_local("vector_db", "test")
+while True:
+    query = input()
+    docs = db.similarity_search_with_score(query, score_threshold=1.2)
+    print(docs)
